2d1c72af5eee2bf7db22627a7062d1edda9c98de
numba19 <zachlegesse@gmail.com>
2024-11-09 00:21:02 -0500

    client: Added ai generateable node

--- client/src/editor.ts
+++ client/src/editor.ts
@@ -2,8 +2,6 @@ import { LGraph, LGraphCanvas, LiteGraph } from "@comfyorg/litegraph";
 export function createGraph(canvasElement: HTMLCanvasElement) {
   const graph = new LGraph();
   const canvas = new LGraphCanvas(canvasElement, graph);
-  const node_const = LiteGraph.createNode("basic/sum");
-  node_const.pos = [200, 200];
-  graph.add(node_const);
   graph.start();
+  return { graph, canvas };
 }
---
+++ client/src/nodes/types/text.ts
@@ -0,0 +1,46 @@
+import { LGraphNode } from "@comfyorg/litegraph";
+import { ensureAbsolutePath } from "../../../../../../../AppData/Local/deno/npm/registry.npmjs.org/@typescript-eslint/typescript-estree/8.13.0/dist/create-program/shared.d.ts";
+
+export class Text extends LGraphNode {
+  value: string = "";
+  hasGenerated: boolean = false;
+  constructor() {
+    super("Generated Text");
+    this.addWidget("text", "string", "", (value) => {
+      this.value = value;
+    });
+    this.addWidget("button", "Generate", "Generate", () => {
+      console.log(this.value);
+      this.generate(this.value);
+    });
+  }
+
+  generate(prompt: string) {
+    fetch("http://localhost:3000/generate", {
+      method: "POST",
+      body: JSON.stringify({
+        prompt,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => response.json())
+      .then(({ response }) => {
+        this.title = response.title;
+        console.log(response.script);
+        const test = eval(`() => {${response.script}}`); // converts arrow function as a string to javascript
+        console.log(test);
+        this.onExecute = test;
+        response.inputs.forEach((input: { label: string; type: string }) => {
+          this.addInput(input.label, input.type);
+        });
+        response.outputs.forEach((output: { label: string; type: string }) => {
+          this.addOutput(output.label, output.type);
+        });
+      }).catch((err) => console.error(err));
+  }
+
+  override onExecute() {
+    this.setOutputData(0, this.value);
+  }
+}
